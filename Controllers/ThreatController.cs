0789fca08edd54636854efb21e2804bde218cd50
Nitran2004 <93067795+Nitran2004@users.noreply.github.com>
2024-03-19 10:31:44 -0500

    ya tiene sonido, falta dirrecionamiento

--- Controllers/ThreatController.cs
+++ Controllers/ThreatController.cs
@@ -1,44 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using SecureAssetManager.Data;
 using SecureAssetManager.Models;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Media;
 
 namespace SecureAssetManager.Controllers
 {
     public class ThreatController : Controller
     {
         private readonly ApplicationDbContext _context;
+		private readonly ILogger<HomeController> _logger;
 
-        public ThreatController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
+		SoundPlayer player = new SoundPlayer();
+		string[] canciones = { "Canciones/Ejemplo.wav", "Canciones/Ejemplo2.wav" }; 
+        int posicion = 0;
+
+		public ThreatController(ApplicationDbContext context, ILogger<HomeController> logger)
+		{
+			_context = context;
+			_logger = logger;
+		}
 
         public async Task<IActionResult> Index()
         {
             return View(await _context.Threats.ToListAsync());
         }
 
-        public IActionResult Create()
-        {
-            return View();
-        }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ThreatOrigin,ThreatDescription,Degradation,Probability")] Threat threat)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(threat);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(threat);
-        }
+
 
         public async Task<IActionResult> Details(int id)
         {
@@ -133,5 +131,43 @@ namespace SecureAssetManager.Controllers
         {
             return _context.Threats.Any(e => e.Id == id);
         }
-    }
+
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Create(string accion)
+		{
+			if (accion == "Página anterior")
+			{
+				this.player = new SoundPlayer(this.canciones[this.posicion]);
+				this.player.LoadAsync();
+				this.player.PlaySync();
+			}
+			if (accion == "Página siguiente" && this.posicion < this.canciones.Length)
+			{
+				this.posicion += 1;
+				this.player = new SoundPlayer(this.canciones[this.posicion]);
+				player.LoadAsync();
+				player.PlaySync();
+			}
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+
+
+
+	}
 }
