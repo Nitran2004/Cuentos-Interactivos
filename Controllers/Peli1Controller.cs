48d35064cd64aceff4ed25eecc943712d8180ef1
Nitran2004 <93067795+Nitran2004@users.noreply.github.com>
2024-06-02 16:22:58 -0500

    libro 10 acabado

--- Controllers/Peli1Controller.cs
+++ Controllers/Peli1Controller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -12,152 +13,33 @@ namespace SecureAssetManager.Controllers
 {
     public class Peli1Controller : Controller
     {
-        private readonly ApplicationDbContext _context;
-
-        public Peli1Controller(ApplicationDbContext context)
-        {
-            _context = context;
-        }
-
-        // GET: Peli1
-        public async Task<IActionResult> Index()
-        {
-              return _context.Peli1 != null ? 
-                          View(await _context.Peli1.ToListAsync()) :
-                          Problem("Entity set 'ApplicationDbContext.Peli1'  is null.");
-        }
-
-        // GET: Peli1/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Peli1 == null)
-            {
-                return NotFound();
-            }
-
-            var peli1 = await _context.Peli1
-                .FirstOrDefaultAsync(m => m.ID == id);
-            if (peli1 == null)
-            {
-                return NotFound();
-            }
-
-            return View(peli1);
-        }
-
-        // GET: Peli1/Create
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Peli1/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        public IActionResult Index()
+        {
+            return View();
+        }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,TipoControl,Efectividad")] Peli1 peli1)
+        public IActionResult Index(string accion)
         {
-            if (ModelState.IsValid)
+            if (accion == "Página siguiente")
             {
-                _context.Add(peli1);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+
+                return RedirectToAction("Create", "Peli2");
             }
-            return View(peli1);
+            return View();
         }
 
-        // GET: Peli1/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
         {
-            if (id == null || _context.Peli1 == null)
-            {
-                return NotFound();
-            }
-
-            var peli1 = await _context.Peli1.FindAsync(id);
-            if (peli1 == null)
-            {
-                return NotFound();
-            }
-            return View(peli1);
-        }
-
-        // POST: Peli1/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,TipoControl,Efectividad")] Peli1 peli1)
-        {
-            if (id != peli1.ID)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(peli1);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!Peli1Exists(peli1.ID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(peli1);
-        }
-
-        // GET: Peli1/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Peli1 == null)
-            {
-                return NotFound();
-            }
-
-            var peli1 = await _context.Peli1
-                .FirstOrDefaultAsync(m => m.ID == id);
-            if (peli1 == null)
-            {
-                return NotFound();
-            }
-
-            return View(peli1);
-        }
-
-        // POST: Peli1/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.Peli1 == null)
-            {
-                return Problem("Entity set 'ApplicationDbContext.Peli1'  is null.");
-            }
-            var peli1 = await _context.Peli1.FindAsync(id);
-            if (peli1 != null)
-            {
-                _context.Peli1.Remove(peli1);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool Peli1Exists(int id)
-        {
-          return (_context.Peli1?.Any(e => e.ID == id)).GetValueOrDefault();
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- Controllers/Peli5Controller.cs
+++ Controllers/Peli5Controller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -12,152 +13,33 @@ namespace SecureAssetManager.Controllers
 {
     public class Peli5Controller : Controller
     {
-        private readonly ApplicationDbContext _context;
-
-        public Peli5Controller(ApplicationDbContext context)
-        {
-            _context = context;
-        }
-
-        // GET: Peli5
-        public async Task<IActionResult> Index()
-        {
-              return _context.Peli5 != null ? 
-                          View(await _context.Peli5.ToListAsync()) :
-                          Problem("Entity set 'ApplicationDbContext.Peli5'  is null.");
-        }
-
-        // GET: Peli5/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Peli5 == null)
-            {
-                return NotFound();
-            }
-
-            var peli5 = await _context.Peli5
-                .FirstOrDefaultAsync(m => m.ID == id);
-            if (peli5 == null)
-            {
-                return NotFound();
-            }
-
-            return View(peli5);
-        }
-
-        // GET: Peli5/Create
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Peli5/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        public IActionResult Index()
+        {
+            return View();
+        }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,TipoControl,Efectividad")] Peli5 peli5)
+        public IActionResult Create(string accion)
         {
-            if (ModelState.IsValid)
+            if (accion == "Página siguiente")
             {
-                _context.Add(peli5);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+
+                return RedirectToAction("Index", "Peli5");
             }
-            return View(peli5);
+            return View();
         }
 
-        // GET: Peli5/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
         {
-            if (id == null || _context.Peli5 == null)
-            {
-                return NotFound();
-            }
-
-            var peli5 = await _context.Peli5.FindAsync(id);
-            if (peli5 == null)
-            {
-                return NotFound();
-            }
-            return View(peli5);
-        }
-
-        // POST: Peli5/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,TipoControl,Efectividad")] Peli5 peli5)
-        {
-            if (id != peli5.ID)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(peli5);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!Peli5Exists(peli5.ID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(peli5);
-        }
-
-        // GET: Peli5/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Peli5 == null)
-            {
-                return NotFound();
-            }
-
-            var peli5 = await _context.Peli5
-                .FirstOrDefaultAsync(m => m.ID == id);
-            if (peli5 == null)
-            {
-                return NotFound();
-            }
-
-            return View(peli5);
-        }
-
-        // POST: Peli5/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.Peli5 == null)
-            {
-                return Problem("Entity set 'ApplicationDbContext.Peli5'  is null.");
-            }
-            var peli5 = await _context.Peli5.FindAsync(id);
-            if (peli5 != null)
-            {
-                _context.Peli5.Remove(peli5);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool Peli5Exists(int id)
-        {
-          return (_context.Peli5?.Any(e => e.ID == id)).GetValueOrDefault();
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
